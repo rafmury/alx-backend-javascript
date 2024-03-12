67e65bd67e48a742a3f5271ac1972af33a0ee933
rafmury <kariukimraphael@gmail.com>
2024-03-12 09:11:23 -0700

    10. More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,23 +2,15 @@
  * Changes the quantity of unique grocery items to 100.
  * @param {Map<String, number>} map - A map of the name of a
  * grocery and its quantity.
- * @returns {Map<String, number>} - Updated map with quantities modified.
  */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-
-  const updatedMap = new Map();
-
   map.forEach((value, key) => {
     if (value === 1) {
-      updatedMap.set(key, 100);
-    } else {
-      updatedMap.set(key, value);
+      map.set(key, 100);
     }
   });
-
-  return updatedMap;
 }
 
