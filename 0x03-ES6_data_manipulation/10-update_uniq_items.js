ae4736e82779dbb1812f5c72a31f2ace4380ee03
rafmury <kariukimraphael@gmail.com>
2024-03-12 09:14:06 -0700

    10. More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,13 +4,13 @@
  * grocery and its quantity.
  */
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
-  map.forEach((value, key) => {
-    if (value === 1) {
-      map.set(key, 100);
-    }
-  });
+	if (!(map instanceof Map)) {
+		throw new Error('Cannot process');
+	}
+	map.forEach((value, key) => {
+		if (value === 1) {
+			map.set(key, 100);
+		}
+	});
 }
 
